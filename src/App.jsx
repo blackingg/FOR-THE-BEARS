d591026b467674b7c8173f28a38d4f888cc47d11
blackingg <odetundemubarak04@gmail.com>
2025-08-18 13:07:30 +0100

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Experience } from "./components/Experience";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { UI } from "./components/UI";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const [started, setStarted] = useState(false);
@@ -36,6 +37,7 @@ function App() {
         started={started}
         setStarted={setStarted}
       />
+      <Analytics />
     </>
   );
 }
